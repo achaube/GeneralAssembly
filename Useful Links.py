5890a8f2b29e7a552f600d13049be248c99120e7
achaube <ashwini.chaube@gmail.com>
2017-02-19 11:07:55 -0800

    restructure

---
+++ Useful Links.py
@@ -0,0 +1,20 @@
+Creating a Repo
+https://help.github.com/articles/create-a-repo/
+
+https://github.com/achaube/GeneralAssembly
+
+Class Repo
+https://github.com/ga-students/DS-SF-32
+
+Git Tutorials
+https://git-scm.com/book/en/v2/Git-Basics-Getting-a-Git-Repository
+https://git-scm.com/docs/git-pull
+
+
+NHANS Data
+https://wwwn.cdc.gov/Nchs/Nhanes/2009-2010/DEMO_F.htm#SDMVSTRA
+https://wwwn.cdc.gov/Nchs/Nhanes/2009-2010/DR1IFF_F.htm#DRDINT
+
+Slack Channel
+https://dat-sf-32.slack.com/messages/general/team/achaube/
+
